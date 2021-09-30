abf4c4df02b0ed0f6b5a2ac394f1e3e7d98e99fc
a.kazantsev <a.kazantsev@tinkoff.ru>
2021-09-30 08:30:04 +0300

    Add tests using JDBC

--- src/test/java/com/acme/dbo/ut/account/ClientControllerRetroFitUnitTest.java
+++ src/test/java/com/acme/dbo/ut/account/ClientControllerRetroFitUnitTest.java
@@ -1,24 +1,37 @@
 package com.acme.dbo.ut.account;
 
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.UUID;
 
 public class ClientControllerRetroFitUnitTest implements ClientsEndpoint{
     private ClientService service;
+    private RequestSpecification request;
+    private Connection connection;
 
     @BeforeEach
-    public void init() {
+    public void init() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:derby://localhost/dbo-db");
         Retrofit retrofit = new Retrofit.Builder()
                 .addConverterFactory(JacksonConverterFactory.create())
                 .baseUrl("http://localhost:8080/dbo/api/")
                 .build();
         service = retrofit.create(ClientService.class);
     }
+
+@AfterEach
+public void disable() throws SQLException {
+        connection.close();
+}
 
     @Test
     public void shouldPost() throws IOException {
